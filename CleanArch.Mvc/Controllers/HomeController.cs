80c0d1f687963e7a05f75dc4aae211a7f9c2cd51
Marinel <info@mdshop.se>
2019-11-23 16:08:49 +0100

    Add Course Domain Model

--- CleanArch.Mvc/Controllers/HomeController.cs
+++ CleanArch.Mvc/Controllers/HomeController.cs
@@ -1,11 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using CleanArch.Mvc.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using CleanArch.Mvc.Models;
+using System.Diagnostics;
 
 namespace CleanArch.Mvc.Controllers
 {
