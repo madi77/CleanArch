352ff986416835a68e5a7a5c8a1fe83e40910656
Marinel <info@mdshop.se>
2019-11-29 17:49:03 +0100

    Presentation cours controller, views styling

---
+++ CleanArch.Mvc/Controllers/CourseController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CleanArch.Application.Interfaces;
+using CleanArch.Application.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CleanArch.Mvc.Controllers
+{
+    
+    public class CourseController : Controller
+    {
+        private ICoursService _coursService;
+        public CourseController(ICoursService coursService)
+        {
+            _coursService = coursService;
+        }
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            CourseViewModel model = _coursService.GetCourses();
+            return View(model);
+        }
+    }
+}
